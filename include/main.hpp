97c7ad2ddf4674da60cfca4d13dc566d0c2696c5
Anthian85 <raghnarok2002@gmail.com>
2023-11-16 23:53:33 +0100

    Added cmake files to compile bullet3. Added bullet headers to main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -11,7 +11,7 @@
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
-//#include <btBulletDynamicsCommon.h>
+#include <btBulletDynamicsCommon.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glad/gl.h>
